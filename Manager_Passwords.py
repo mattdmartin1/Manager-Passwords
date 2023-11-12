14dfec1dd378bc5524c93a255ba0b35505e565ec
mattdmartin1 <mattdmartin1@gmail.com>
2023-11-12 12:33:11 -0800

    Added text file

--- Manager_Passwords.py
+++ Manager_Passwords.py
@@ -7,10 +7,16 @@
 master_pwd = input('What is the master password?: ')
 
 def view():
+    
     pass
 
 def add():
-    pass
+    name = input('Account Name: ')
+    pwd = input('Enter password: ')
+    
+    with open ('new_txt.file', 'a') as f:
+        f.write(name + '|' + pwd + '\n')
+        
 
 while True:
     mode = input('Would you like to add a new password or existing ones (view, add), press q to quit? ').lower()
