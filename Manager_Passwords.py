9d49e19485a073b11532582c32dfa20876240ced
mattdmartin1 <mattdmartin1@gmail.com>
2023-11-12 12:22:47 -0800

    Third Commit

--- Manager_Passwords.py
+++ Manager_Passwords.py
@@ -1,2 +1,28 @@
 # Master Password
-# 
+# view function
+# add function
+# While True
+# add of view existing loop
+
+master_pwd = input('What is the master password?: ')
+
+def view():
+    pass
+
+def add():
+    pass
+
+while True:
+    mode = input('Would you like to add a new password or existing ones (view, add), press q to quit? ').lower()
+    if mode == 'quit':
+        break
+    
+    if mode == 'view':
+        view()
+    
+    elif mode == 'add':
+        add()
+    
+    else:
+        print('Incorrect mode. ')
+        continue
